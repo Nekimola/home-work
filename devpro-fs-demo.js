ca3e3e3f41b323384a1a5923976d102d4a3d36f9
Oleg <margin2call@mail.ru>
2016-07-20 01:49:55 +0300

    First commit

---
+++ devpro-fs-demo.js
@@ -0,0 +1,70 @@
+"use strict";
+
+const fs = require('fs');
+const http = require('http');
+
+const url = require('url');
+const path = require('path');
+
+
+const server = http.createServer((req, res) => {
+	const data = url.parse(req.url, true);
+
+	if (req.url  === '/favicon.ico') {
+		// тяжелый вздох
+		res.statusCode = 204;
+		res.end();
+		return;
+	}
+
+	const dir = data.query.path || '/';
+
+	fs.readdir(dir, (err, data) => {
+		if (err) {
+			res.statusCode = 500;
+			res.end(err.toString());
+			return;
+		}
+
+		const files = data.map(file => {
+            let result = { name: file };
+
+			try {
+				return Object.assign(result, {
+					isDirectory: fs.statSync(path.join(dir, file)).isDirectory()
+				});
+			} catch (e) {
+				return result;
+			}
+		});
+
+        var promises = data.map(file => {
+            let result = { name: file };
+
+            try {
+                return new Promise((resolve, reject) => {
+                    fs.stat(path.join(dir, file), (err, stats) => {
+                        if (err) {
+                            reject(err);
+                            return;
+                        }
+                        resolve(Object.assign(result, { isDirectory: stats.isDirectory() }));
+                    });
+                });
+
+            } catch (e) {
+                return Promise.resolve(result);
+            }
+        });
+
+        Promise.all(promises)
+            .then(files => {
+                res.writeHead(200, { 'Content-type': 'application/json' });
+                res.end(JSON.stringify(files));
+            });
+	});
+
+});
+
+server.listen(3000);
+
